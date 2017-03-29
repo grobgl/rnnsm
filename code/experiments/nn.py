08763194d55efea53dcf36d7ee1bf73a6649c6e4
Georg Lukas Grob <grobgl@gmail.com>
2017-03-29 14:08:22 +0100

    Use tensorboard

--- code/experiments/nn.py
+++ code/experiments/nn.py
@@ -10,8 +10,8 @@ import datetime
 
 from keras.models import Sequential
 from keras.callbacks import ProgbarLogger
-from keras.layers import Dense
-from keras.callbacks import Callback, LambdaCallback
+from keras.layers import Dense, Dropout
+from keras.callbacks import Callback, LambdaCallback, TensorBoard
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -53,12 +53,13 @@ def deeper_model():
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
-def fit_model(model, batch_size=1000, nb_epoch=100000, log_every=1000):
+def fit_model(model, batch_size=1000, nb_epoch=10000, log_every=1000):
     hist = model.fit(
 	X_train, y_train,
 	batch_size=batch_size, nb_epoch=nb_epoch,
 	verbose=0
-	, callbacks=[LambdaCallback(on_epoch_end=lambda epoch, logs: print(epoch, logs) if (epoch % log_every== 0) else 0)]
+        , callbacks=[TensorBoard(log_dir='../../logs', histogram_freq=100)]
+	# , callbacks=[LambdaCallback(on_epoch_end=lambda epoch, logs: print(epoch, logs) if (epoch % log_every== 0) else 0)]
     )
     return hist
 
