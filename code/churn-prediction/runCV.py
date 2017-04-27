3e05aa9a1398920c977cb87d3d380e4adf681cf3
Georg Grob <georg.grob13@imperial.ac.uk>
2017-04-27 08:02:26 +0100

    random forest resutls

--- code/churn-prediction/runCV.py
+++ code/churn-prediction/runCV.py
@@ -4,14 +4,14 @@ import sys
 sys.path.insert(0, '../utils')
 # from plot_format import *
 
-from logistic_regression import *
+# from logistic_regression import *
 from random_forest import *
 
 def run():
     # runL1GridSearch()
-    runL2GridSearch()
-    runFeatureElimination(includeFeat='all')
-    runFeatureElimination(includeFeat='avg')
-    runFeatureElimination(includeFeat='wght')
+    # runL2GridSearch()
+    # runFeatureElimination(includeFeat='all')
+    # runFeatureElimination(includeFeat='avg')
+    # runFeatureElimination(includeFeat='wght')
     runGridSearch()
 
